62cce370b2e0a94bec9a69a222b76e5eb1c5bf53
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-04-22 18:05:29 +0530

    endpoint added2

--- index.js
+++ index.js
@@ -4,33 +4,24 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 require('dotenv').config();
 
-
-app.get("/api", (req, res) => {
-   res.send("welcome") 
-})
-
-
 app.get('/ogdata', async (req, res) => {
- 
     const id = "1234"
-
     const post = {
         title: "Hello",  
         productExperience: "demo",
-        url:"https://us06st2.zoom.us/static/6.3.12506/image/Zoom_Blue_Logo.png"
-        
+        url:"https://us06st2.zoom.us/static/6.3.12506/image/Zoom_Blue_Logo.png"  
      }
 
   try {
     const ogTags = {
         title: post.title,
         description: post.productExperience,
-        image: post.url, // Assumes the first media item is the main image
+        image: post.url,
         url: `https://www.mybranz.com/posts/${id}`,
         type: "article"
       };
       for (const [key, value] of Object.entries(ogTags)) {
-        res.setHeader(key, value);
+        res.setHeader(`og:${key}`, value);
       }
 
       return res.json(post).status(200);
